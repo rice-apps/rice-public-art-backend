9596015d774f852250b913eedafb09af67ba718c
Ryan Knightly <reknightly@gmail.com>
2020-02-08 15:01:28 -0600

    Sort past and future events

--- routes/general.js
+++ routes/general.js
@@ -52,17 +52,26 @@ router.get('/allEvents', (req, res) => {
 })
 
 router.get('/events', (req, res) => {
+    // Query Time Period
     var year = req.query.year
-    var month = req.query.month
-    if (month.length < 2) {month = "0" + month}
+    var month = req.query.month.padStart(2, '0');
+
+    // Today's Date
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0
+    var yyyy = today.getFullYear();
+    var todayDateStr = yyyy + "-" + mm + "-" + dd;
     event.getEvents()
         .then(events => {
             data = events.items.map(event => {
                 let fields = event.fields;
                 fields.image = "https:" + fields.image.fields.file.url;
+                fields.isPastEvent = fields.date.localeCompare(todayDateStr) < 0
                 return fields;
             })
             data = data.filter(event => event.date.includes(year + "-" + month))
+            data = data.sort((a, b) => a.isPastEvent < b.isPastEvent || a.date.localeCompare(b.date))
             return res.send({
                 success: true,
                 data: data
